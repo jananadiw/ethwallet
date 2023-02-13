0ac7a2b1c85b1ea0294c9a8761cda237d51431ee
Jananadi Wedagedara <jananadi92@gmail.com>
2023-02-13 19:45:35 +0900

    feat: Connect metamask wallet

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,82 @@
-import Image from 'next/image'
-let Web3 = require('web3');
+'use client'
+
+import React, {useEffect, useState} from 'react';
+const Web3 = require('web3');
 
 export default function Home() {
-  let web3 = new Web3('http://localhost:7545');
-  web3.eth.getAccounts(console.log);
+  const [walletAddress, setWalletAddress] = useState('');
+
+  // Find connected wallet when page reloads
+  useEffect(() => {
+    getConnectedWallet();
+    swichAccountListener();
+  })
+
+  // // Connect Metamast Wallet
+  const connectWallet = async() => {
+    if(typeof window != 'undefined' && typeof window.ethereum != 'undefined') {
+      try {
+        const web3 = new Web3(window.ethereum)
+        const accounts = await web3.eth.requestAccounts();
+        setWalletAddress(accounts[0]);
+        console.log(accounts)
+      }catch(e) {
+        console.error(e)
+      }
+    } else {
+      console.log('metamask is not installed')
+    }
+  }
+
+  // // Get Currently connect Metamask Wallet
+    const getConnectedWallet = async() => {
+      if(typeof window != 'undefined' && typeof window.ethereum != 'undefined') {
+        try {
+          // const accounts = await window.ethereum.request({method: 'eth_accounts'});
+          const web3 = new Web3(window.ethereum)
+          const accounts = await web3.eth.getAccounts();
+          if(accounts.length > 0) {
+            setWalletAddress(accounts[0]);
+            console.log(accounts[0])
+          }else {
+            // we've lost connection to the wallet
+            console.log('Connect to Metamask')
+          }
+        }catch(e) {
+          console.error(e)
+        }
+      } else {
+        console.log('metamask is not installed')
+      }
+    }
+
+  // // Switch Account
+  const swichAccountListener = () => {
+    if(typeof window != 'undefined' && typeof window.ethereum != 'undefined') {
+      window.ethereum.on('accountsChanged', (accounts: any) => {
+          setWalletAddress(accounts[0]);
+          console.log(accounts[0]);
+      });
+    } else {
+      setWalletAddress('');
+      console.log('metamask is not installed')
+    }
+  }
+
+
+  // Get Trucated wallet address
+  const getTrucatedWalletAddress = () => {
+    return walletAddress && walletAddress.length > 0 ? `Connected: ${walletAddress.substring(0,6)}...${walletAddress.substring(38)}` : `Connect Wallet`
+  }
+
   return (
     <main>
-      <div>
-       <p>Welcome to Wallet</p>
-      </div>
+    <div className='font-mono container m-auto p-8'>
+       <button className="float-right bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={connectWallet}>{getTrucatedWalletAddress()}</button>
+       <div>
+        <p className='text-2xl'>Welcome to My Ether Wallet</p>
+       </div>
+    </div>
     </main>
   )
 }
