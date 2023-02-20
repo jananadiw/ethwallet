2ee91aea57976770a96a88746d7735d438b59662
Jananadi Wedagedara <jananadi92@gmail.com>
2023-02-20 22:25:50 +0900

    lint: Fix lint after setting up prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,16 @@
-import '@/styles/globals.scss'
+import '@/styles/globals.scss';
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+    children: React.ReactNode;
+}): React.ReactElement {
+    return (
+        <html lang="en">
+            <head />
+            {/* header */}
+            <body>{children}</body>
+            {/* footer */}
+        </html>
+    );
 }
