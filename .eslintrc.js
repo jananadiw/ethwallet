98cfa51395042372301710430c603ed78a154adb
Jananadi Wedagedara <jananadi92@gmail.com>
2023-02-20 18:11:08 +0900

    lint: Setup Prettier formatter rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,25 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript',
-    'plugin:@next/next/recommended'
-  ],
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: [
-    'react'
-  ],
-  rules: {
-  }
-}
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'plugin:react/recommended',
+        'standard-with-typescript',
+        'plugin:@next/next/recommended',
+        'prettier',
+    ],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
+    },
+    plugins: ['react', 'prettier'],
+    rules: {
+        'prettier/prettier': ['error'],
+        'react/react-in-jsx-scope': 'off',
+        'react/prop-styles': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'warn',
+    },
+};
