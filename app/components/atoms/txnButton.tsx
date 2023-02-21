4463da5324292be3f66bb8bef8ad0e21450c976a
Jananadi Wedagedara <jananadi92@gmail.com>
2023-02-22 00:32:32 +0900

    lint: Fix lint errors

--- app/components/atoms/txnButton.tsx
+++ app/components/atoms/txnButton.tsx
@@ -1,8 +1,8 @@
-export function TxnButton({ data }: any): React.ReactElement {
+export function TxnButton({ data, onClick }: any): React.ReactElement {
     const btnText = data;
     return (
         <>
-            <button className="bg-gray-600 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+            <button className="bg-gray-600 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded" onClick={onClick}>
                 {btnText}
             </button>
         </>
