b26abc89be9f1d163c895ed1f7bdbcae1048db11
Jananadi Wedagedara <jananadi92@gmail.com>
2023-02-23 19:54:47 +0900

    lint: Fix linting

--- truffle-config.js
+++ truffle-config.js
@@ -1,21 +1,17 @@
-require("dotenv").config();
+require('dotenv').config();
 const { MNEMONIC, PROJECT_ID } = process.env;
 
-const HDWalletProvider = require("@truffle/hdwallet-provider");
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
     networks: {
         development: {
-            host: "127.0.0.1", // Localhost (default: none)
+            host: '127.0.0.1', // Localhost (default: none)
             port: 9545, // Standard Ethereum port (default: none)
-            network_id: "*", // Any network (default: none)
+            network_id: '*', // Any network (default: none)
         },
         goerli: {
-            provider: () =>
-                new HDWalletProvider(
-                    MNEMONIC,
-                    `https://goerli.infura.io/v3/${PROJECT_ID}`,
-                ),
+            provider: () => new HDWalletProvider(MNEMONIC, `https://goerli.infura.io/v3/${PROJECT_ID}`),
             network_id: 5, // Goerli's id
             confirmations: 2, // # of confirmations to wait between deployments. (default: 0)
             timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
@@ -24,7 +20,7 @@ module.exports = {
     },
     compilers: {
         solc: {
-            version: "0.8.17",
+            version: '0.8.17',
         },
     },
 };
