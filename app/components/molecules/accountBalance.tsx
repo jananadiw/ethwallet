b1364c478a5010a0a32b68cb8f7ed7a2ef3e53fd
Jananadi Wedagedara <jananadi92@gmail.com>
2023-02-23 20:00:59 +0900

    improvement: Update token balance amount UI

--- app/components/molecules/accountBalance.tsx
+++ app/components/molecules/accountBalance.tsx
@@ -1,18 +1,18 @@
-interface AccoutBalance {
+interface IAccountBalance {
     props: {
         ether: number;
         token: string;
     };
 }
 
-export function AccountBalance({ props }: AccoutBalance): React.ReactElement {
+export function AccountBalance({ props }: IAccountBalance): React.ReactElement {
     const balance = props;
 
     return (
         <>
             <div className="text-xl m-8 p-8 text-center leading-loose">
                 <p>{`Ether Balance: ${balance.ether}`}</p>
-                <p>{`Token Balance: ${balance.token}`}</p>
+                <p>{`Token Balance: ${Math.round(Number(balance.token) * 1000) / 1000} MET`}</p>
             </div>
         </>
     );
